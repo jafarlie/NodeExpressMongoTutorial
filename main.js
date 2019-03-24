1647e3ab1c834bec1d00ca30e0f9a445f11877bb
Elvin <jafarli.elvin1997@gmail.com>
2019-03-24 12:24:10 -0400

    Now able to send html page

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+const path = require('path');
 
 app.get('/', (req, res) => {
-    res.send("hello");
+    res.sendFile(path.join(__dirname + '/Public/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
